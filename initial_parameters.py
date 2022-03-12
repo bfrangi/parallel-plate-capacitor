f1e9969b4fb1e03aec48e7479abdd5dcb8266530
bfrangi <bernat.frangi@gmail.com>
2022-03-12 21:31:53 +0100

    update

--- initial_parameters.py
+++ initial_parameters.py
@@ -1,9 +1,31 @@
-x_min = 80
-x_max = -80
+# Dimensions of the surrounding box (conductor):
+Lx = 10#cm (x side of the box)
+x_min = -Lx/2
+x_max = Lx/2
 Mx = 101
-y_min = 80
-y_max = -80
+Ly = 15#cm (y side of the box)
+y_min = -Ly/2
+y_max = Ly/2
 My = 151
-z_min = 80
-z_max = -80
+Lz = 30#cm (z side of the box)
+z_min = -Lz/2
+z_max = Lz/2
 Mz = 301
+
+# Potential of the surrounding box (conductor):
+Vbox = 0#V
+
+# Position and dimensions of the ppcs:
+d = 1#cm (distance between plates)
+x1 = -d/2# (x position of the first plate)
+x2 = d/2# (x position of the second plate)
+lz = 10#cm (length of the z side for both plates)
+zmax = lz/2# (end of the z side for both plates)
+zmin = -lz/2# (start of the z side for both plates)
+ly = 5#cm (length of the y side for both plates)
+ymax = ly/2# (end of the y side for both plates)
+ymin = -ly/2# (start of the y side for both plates)
+
+# Potential at the ppcs:
+V1 = 10#V
+V2 = -5#V
--- simulation.py
+++ simulation.py
@@ -1,17 +1,44 @@
 from initial_parameters import *
+import numpy as np
 
 # SPACE AND TIME VECTORS
 def generate_mesh(vec_min, vec_max, Mv):
-    v = []
-    Dv = (vec_max - vec_min) / (Mv - 1)
-    for k in range(Mv):
-        v.append( vec_min + k*Dv )
-    return np.array(v)
+	v = []
+	Dv = (vec_max - vec_min) / (Mv - 1)
+	for k in range(Mv):
+		v.append( vec_min + k*Dv )
+	return np.array(v)
 
+def create_mesh(x, y, z):
+	#x = [num for num in range(-5, 6)]
+	#y = [num for num in range(-5, 6)]
+	#z = [num for num in range(-5, 6)]
+	mesh = []
 
+	for i in range(len(x)):
+		this_x = []
+		for j in range(len(y)):
+			this_y = []
+			for k in range(len(z)):
+				this_y.append(potential(i, j, k))
+				count += 1
+			this_x.append(this_y)
+		mesh.append(this_x)
+	return mesh
+
+def potential(i, j, k):
+	return 0
 
 # MAIN FUNCTION
 if __name__=="__main__":
 	x = generate_mesh(x_min, x_max, Mx)
-    y = generate_mesh(y_min, y_max, My)
-    z = generate_mesh(z_min, z_max, Mz)
+	y = generate_mesh(y_min, y_max, My)
+	z = generate_mesh(z_min, z_max, Mz)
+	#mesh = lambda xi, yi, zi: (x[xi], y[yi], z[zi])
+	
+	mesh = create_mesh(x, y, z)
+
+	#for i in range(len(x)):
+	#	for j in range(len(y)):
+	#		for k in range(len(z)):
+	#			print(mesh(i, j, k))
