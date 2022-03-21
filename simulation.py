29ca1dead7096089556b3ea36354c5b3f7e52bf8
bfrangi <bernat.frangi@gmail.com>
2022-03-21 20:06:45 +0100

    changed lists to numpy arrays + added function to store matrix

--- simulation.py
+++ simulation.py
@@ -1,13 +1,16 @@
 from initial_parameters import *
 import numpy as np
+import re
+from numba import jit, njit
+from time import time as t
 
 # SPACE AND TIME VECTORS
-def generate_mesh(vec_min, vec_max, Mv, Dv):
-	v = []
+def generate_vector(vec_min, vec_max, Mv, Dv):
+	v = np.array([])
 	#Dv = (vec_max - vec_min) / (Mv - 1)
 	for k in range(Mv):
-		v.append( vec_min + k*Dv )
-	return np.array(v)
+		v = np.append(v, [vec_min + k*Dv])
+	return v
 
 def create_mesh(x, y, z):
 	mesh = []
@@ -19,7 +22,7 @@ def create_mesh(x, y, z):
 				this_y.append(initial_potential(j, k, l))
 			this_x.append(this_y)
 		mesh.append(this_x)
-	return mesh
+	return np.array(mesh)
 
 def initial_potential(j, k, l):
 	edge_x = [0, Mx - 1]
@@ -32,76 +35,94 @@ def initial_potential(j, k, l):
 	plates_z = range(int((zmin-z_min)/Dz), int((zmax-z_min)/Dz) + 1)
 
 	if j in edge_x or k in edge_y or l in edge_z:
-		return Vbox
+		return float(Vbox)
 	elif k in plates_y and l in plates_z:
 		if j == plate1_x:
-			return V1
+			return float(V1)
 		elif j == plate2_x:
-			return V2
+			return float(V2)
 		else: 
-			return 0
+			return 0.
 	else:
-		return 0
+		return 0.
 
 def find_center(mesh):
 	x_len = len(mesh)
-	y_len = len(mesh[0])
-	z_len = len(mesh[0][0])
+	y_len = len(mesh[0, :, :])
+	z_len = len(mesh[0, 0, :])
 	return (int(x_len/2), int(y_len/2), int(z_len/2))
 	
-def print_z_cut(l, mesh, show_zeros=False):
+def print_z_cut(l, mesh, show_zeros=False, rounding='no'):
 	x_len = len(mesh)
-	y_len = len(mesh[0])
+	y_len = len(mesh[0, :, :])
 	for j in range(x_len):
 		for k in range(y_len):
 			if show_zeros:
-				print(mesh[j][k][l], end=" ")
+				if rounding=='no':
+					print(mesh[j, k, l], end=" ")
+				else:
+					print(round(mesh[j, k, l], rounding), end=" ")
 			else:
-				m = mesh[j][k][l]
+				m = mesh[j, k, l]
 				if m != 0:
-					print(m, end=" ")
+					if rounding=='no':
+						print(m, end=" ")
+					else:
+						print(round(m, rounding), end=" ")
 				else:
 					print(" ", end="")
 		print("")
 
-def print_y_cut(k, mesh, show_zeros=False):
+def print_y_cut(k, mesh, show_zeros=False, rounding='no'):
 	x_len = len(mesh)
-	z_len = len(mesh[0][0])
+	z_len = len(mesh[0, 0, :])
 	for j in range(x_len):
 		for l in range(z_len):
 			if show_zeros:
-				print(mesh[j][k][l], end=" ")
+				if rounding=='no':
+					print(mesh[j, k, l], end=" ")
+				else:
+					print(round(mesh[j, k, l], rounding), end=" ")
 			else:
-				m = mesh[j][k][l]
+				m = mesh[j, k, l]
 				if m != 0:
-					print(m, end=" ")
+					if rounding=='no':
+						print(m, end=" ")
+					else:
+						print(round(m, rounding), end=" ")
 				else:
 					print(" ", end="")
 		print("")
 
-def print_x_cut(j, mesh, show_zeros=False):
+def print_x_cut(j, mesh, show_zeros=False, rounding='no'):
 	y_len = len(mesh[0])
-	z_len = len(mesh[0][0])
+	z_len = len(mesh[0, 0, :])
 	for k in range(y_len):
 		for l in range(z_len):
 			if show_zeros:
-				print(mesh[j][k][l], end=" ")
+				if rounding=='no':
+					print(mesh[j, k, l], end=" ")
+				else:
+					print(round(mesh[j, k, l], rounding), end=" ")
 			else:
-				m = mesh[j][k][l]
-				if m!=0:
-					print(m, end=" ")
+				m = mesh[j, k, l]
+				if m != 0:
+					if rounding=='no':
+						print(m, end=" ")
+					else:
+						print(round(m, rounding), end=" ")
 				else:
 					print(" ", end="")
 		print("")
 
 def stencil_average(j, k, l, mesh):
-	top = mesh[j][k][l + 1]
-	bottom = mesh[j][k][l - 1]
-	left = mesh[j - 1][k][l]
-	right = mesh[j + 1][k][l]
-	forward = mesh[j][k + 1][l]
-	backward = mesh[j][k - 1][l]
-	current = mesh[j][k][l]
+	top = mesh[j, k, l + 1]
+	bottom = mesh[j, k, l - 1]
+	left = mesh[j - 1, k, l]
+	right = mesh[j + 1, k, l]
+	forward = mesh[j, k + 1, l]
+	backward = mesh[j, k - 1, l]
+	current = mesh[j, k, l]
 
 	new = (top + bottom + left + right + forward + backward)/6
 	residual = abs(current - new)
@@ -130,34 +151,44 @@ def updated_potential(j, k, l, mesh):
 	else:
 		return stencil_average(j, k, l, mesh)
 
+#@njit
 def update_potential(mesh):
 	x_len = len(mesh)
-	y_len = len(mesh[0])
-	z_len = len(mesh[0][0])
+	y_len = len(mesh[0, :, :])
+	z_len = len(mesh[0, 0, :])
 	max_res = 0
 	for j in range(x_len):
 		for k in range(y_len):
 			for l in range(z_len):
-				mesh[j][k][l], residual = updated_potential(j, k, l, mesh)
+				mesh[j, k, l], residual = updated_potential(j, k, l, mesh)
 				if residual > max_res:
 					max_res = residual
 
 	return mesh, max_res
 
-# MAIN FUNCTION
-if __name__=="__main__":
-	x = generate_mesh(x_min, x_max, Mx, Dx)
-	y = generate_mesh(y_min, y_max, My, Dy)
-	z = generate_mesh(z_min, z_max, Mz, Dz)
-	#mesh = lambda xi, yi, zi: (x[xi], y[yi], z[zi])
+def export_matrix(matrix, filename='potential_matrix.npy'):
+	f = open(filename, 'wb')
+	np.save(f, matrix)
+	f.close()
+
+def import_matrix(filename='potential_matrix.npy'):
+	f = open(filename, 'rb')
+	matrix = np.load(f)
+	f.close()
+	return matrix
+
+def compute_potential_matrix(save=True):
+	x = generate_vector(x_min, x_max, Mx, Dx)
+	y = generate_vector(y_min, y_max, My, Dy)
+	z = generate_vector(z_min, z_max, Mz, Dz)
 
 	mesh = create_mesh(x, y, z)
-	mesh_center = find_center(mesh)	
-
+	
 	prev_residual = 0
 	residual = 1000# (any value different to the prev_residual)
 	reached_tolerance = False
 	iteration_number = 1
+
 	while residual != prev_residual and not reached_tolerance:
 		prev_residual = residual
 		if iteration_number == 1:
@@ -167,9 +198,37 @@ if __name__=="__main__":
 		mesh, residual = update_potential(mesh)
 		iteration_number += 1
 		if residual < Rtol:
-			reached_tolerance = True	
+			reached_tolerance = True
+	print("Done. Final Residual: " + str(round(residual, 3)))
+	if save:
+		export_matrix(mesh)
 
-	print_z_cut(mesh_center[2], mesh)
+	return mesh
+
+# MAIN FUNCTION
+if __name__=="__main__":
+	print("Choose an option:")
+	print("1. Compute the potential matrix from scratch")
+	print("2. Import the potential matrix from a file")
+	choice = input("Enter [1] or [2]: ")
+	if choice == "1":
+		start_time = t()
+		mesh = compute_potential_matrix()
+		computation_time = t() - start_time
+		print("Time taken:", computation_time, "s")
+
+		#mesh_center = find_center(mesh)	
+		#print(mesh)
+	elif choice == "2":
+		start_time = t()
+		mesh = import_matrix()
+		import_time = t() - start_time
+		print("Time taken:", import_time, "s")
+
+		#mesh_center = find_center(mesh)	
+		#print(mesh)
+	else:
+		print("Invalid Choice")
 
 
 
